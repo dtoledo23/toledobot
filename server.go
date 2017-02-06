cc0dd3b596134e68e65ad5a09a6bd996e630cdda
Diego Toledo <toledo.diego09@gmail.com>
2017-02-05 20:53:49 -0600

    Add dotenv for loading environment variables

--- server.go
+++ server.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+  "os"
   "fmt"
   "log"
-  "app/routes"
   "net/http"
+  "strings"
+  "app/routes"
+  "github.com/joho/godotenv"
+
 )
 
 func main() {
+  // Load environment variables.
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
 
-    // Run server.
-    log.Fatal(http.ListenAndServe(":8080", routes.Router()))
+  // Run server.
+  // Address = HOST:PORT
+  address := strings.Join([]string{os.Getenv("HOST"), os.Getenv("PORT")}, ":")
+  fmt.Println("ToledoBot running on ", address)
+  log.Fatal(http.ListenAndServe(address, routes.Router()))
+
 }
