cc092850b65b0227d89fdc15ba0b3ab9b3a7887c
Diego Toledo <toledo.diego09@gmail.com>
2017-02-26 18:15:40 -0600

    Small changes made by linter

--- server.go
+++ server.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-  "os"
-  "log"
-  "net/http"
-  "strings"
-  "app/routes"
-  "github.com/joho/godotenv"
+	"app/routes"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 
+	"github.com/joho/godotenv"
 )
 
 func main() {
-  // Load environment variables.
-  err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+	// Load environment variables.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-  // Run server.
-  // Address = HOST:PORT
-  address := strings.Join([]string{os.Getenv("HOST"), os.Getenv("PORT")}, ":")
-  log.Println("ToledoBot running on ", address)
-  log.Fatal(http.ListenAndServe(address, routes.Router()))
+	// Run server.
+	// Address = HOST:PORT
+	address := strings.Join([]string{os.Getenv("HOST"), os.Getenv("PORT")}, ":")
+	log.Println("ToledoBot running on ", address)
+	log.Fatal(http.ListenAndServe(address, routes.Router()))
 
 }
