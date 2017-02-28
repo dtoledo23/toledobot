13a9f437144424b8131b0f94fc00800b7efec720
Diego Toledo <toledo.diego09@gmail.com>
2017-02-28 13:45:40 -0600

    Implemented bot logic

--- vendor/app/bot/bot.go
+++ vendor/app/bot/bot.go
@@ -5,8 +5,34 @@ import (
 	"fmt"
 )
 
+const (
+	witAPI = "api.wit.ai"
+)
+
+type answer struct {
+	Type     string            `json:"type"`
+	Payload  string            `json:"payload"`
+	Entities map[string]string `json:"type"`
+}
+
 // HandleMessage defines what to do when receiving a message.
 func HandleMessage(message string, sender platforms.Sender) {
 	fmt.Println("Ill do my best to answer to ", message)
-	sender.SendText("Hi")
+	var response string
+
+	switch entities := getEntities(message); entities["intent"] {
+	case "greetings":
+		response = "Hola! Soy TrokoBot"
+
+	case "address":
+		response = "Paseo del Hospicio #22, San Juan de Dios 44360 Guadalajara local 2151"
+
+	case "schedule":
+		response = "Lunes a Sabado de 9:00am a 7:00pm"
+
+	default:
+		response = "Lo siento. No pude entender."
+	}
+
+	sender.SendText(response)
 }
---
+++ vendor/app/bot/wit.go
@@ -0,0 +1,84 @@
+package bot
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+const (
+	confidenceThreshold = 0.7
+)
+
+type witMessage struct {
+	Entities map[string][]struct {
+		Value      string  `json:"value"`
+		Confidence float64 `json:"confidence"`
+	} `json:"entities"`
+}
+
+func getEntities(message string) map[string]string {
+	entities := make(map[string]string)
+
+	witResponse, err := callWitAPI(message)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	for entity, values := range witResponse.Entities {
+		var bestValue string
+		maxConfidence := float64(0)
+
+		for _, entityResult := range values {
+			if (entityResult.Confidence > confidenceThreshold) &&
+				(entityResult.Confidence > maxConfidence) {
+				bestValue = entityResult.Value
+				maxConfidence = entityResult.Confidence
+			}
+		}
+
+		entities[entity] = bestValue
+	}
+
+	return entities
+}
+
+func callWitAPI(message string) (*witMessage, error) {
+	// Build request
+	q := url.Values{}
+	q.Add("q", message)
+
+	url := url.URL{
+		Scheme:   "https",
+		Host:     witAPI,
+		Path:     "/message",
+		RawQuery: q.Encode(),
+	}
+
+	req, err := http.NewRequest("GET", url.String(), nil)
+	req.Header.Add("Authorization", "Bearer "+os.Getenv("WIT_TOKEN"))
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Perform request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse JSON response
+	defer resp.Body.Close()
+	var entities witMessage
+
+	err = json.NewDecoder(resp.Body).Decode(&entities)
+	if err != nil {
+		return nil, err
+	}
+
+	return &entities, nil
+}
